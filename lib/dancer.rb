5f091cac7a7f988be6dc2de9b39e701a27967411
github username <github email address>
2019-09-23 10:27:48 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,15 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+class Dancer
+  #include Dance #includes the dance_module.rb as instance methods
+  # extend MetaDancing #includes class_mehtods_module as class methods
+
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance #includes the dance_module.rb as instance methods
+  extend MetaDancing
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
